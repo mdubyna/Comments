3dd3ad320f57e5db320e6ed891fde7fb1f11fad1
Mykola <mykola22dubyna@gmail.com>
2024-05-19 19:31:37 +0300

    Fix comment in build.sh

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
-# Convert static asset file
+# Convert static asset files
 python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
