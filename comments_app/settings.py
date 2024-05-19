09be7223cb39f4d2bbbf25b0d8d389434240489e
Mykola <mykola22dubyna@gmail.com>
2024-05-19 19:56:46 +0300

    Update ALLOWED_HOSTS

--- comments_app/settings.py
+++ comments_app/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "websocketking.com"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "websocketking.com", "comments-3g0k.onrender.com"]
 
 # Application definition
 
