3c46214b818203dfeb84488a03294a6f3493a723
Mykola <mykola22dubyna@gmail.com>
2024-05-19 19:51:13 +0300

    Upgrade static files managing

--- comments_app/settings.py
+++ comments_app/settings.py
@@ -141,6 +141,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, "static")
